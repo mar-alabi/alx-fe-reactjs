ad82677b0f859aef065aac952e8e404f3e2c4b46
morrryyyy <morrryyyy20@gmail.com>
2025-12-08 10:24:10 +0100

    edit file paths

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./MainContent";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
@@ -7,7 +11,9 @@ function App() {
 
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
